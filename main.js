23e46290d092913a03bb7dd2276e794adf782a77
Meowmeowc <lalachka26@mail.ru>
2023-02-06 14:53:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// let a = 1;
+// let b = prompt('Введите число');
+
+// switch (true) {
+// 	case (a === b):
+// 		alert('да')
+// 		break;
+// 	case (a != b):
+// 		alert('нет')
+// 		break;
+// 	default:
+// 		alert('не сработало')
+// }
+
+// let arg = prompt('Введите число')
+// switch (arg) {
+// 	case '1':
+// 	case '2':
+// 	case '3':
+// 		alert('Зима')
+// 		break;
+// 	case '4':
+// 	case '5':
+// 	case '6':
+// 		alert('Весна')
+// 		break;
+// 	case '7':
+// 	case '8':
+// 	case '9':
+// 		alert('Лето')
+// 		break;
+// 	case '10':
+// 	case '11':
+// 	case '12':
+// 		alert('Осень')
+// 		break;
+// 	default:
+// 		alert('Нет столько месяцев')
+// }
+
+// let one = '1';
+// let two = null;
+// let three = '3'
+// alert(one ?? two ?? three ?? 'Неизвестное число')
+
+
+// let number = +prompt('Введите число от 0 до 5', '');
+
+// switch (number) {
+// 	case 0:
+// 		alert('Вы ввели число 0')
+// 		break;
+// 	case 1:
+// 		alert('Вы ввели число 1')
+// 		break;
+// 	case 2:
+// 	case 3:
+// 		alert('Вы ввели 2 или 3')
+// 		break;
+// 	default:
+// 		alert('Вы ввели некорректное значение')
+// }
